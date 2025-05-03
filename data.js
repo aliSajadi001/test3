d126dbcb218e0014c56a02ea903993d3c4cab211
aliSajadi001 <alisajadi001010@gmail.com>
2025-05-03 20:19:09 +0330

    first commit

---
+++ data.js
@@ -0,0 +1,84 @@
+export const tabs = [
+  {
+    id: 0,
+    title: "همه",
+    images: [
+      "public/baner/workforce-feature.jpg__992x558_q85_crop-smart_subsampling-2_upscale.jpg",
+      "public/baner/unnamed.jpg",
+      "public/baner/The Importance of Water in a Laboratory.png",
+      "public/baner/pexels-pixabay-40568.jpg",
+      "public/baner/nurses-patients-care-procedures.webp",
+      "public/baner/istockphoto-1012405368-612x612.jpg",
+      "public/baner/lab-quality-assurance-l.jpg",
+    ],
+  },
+  {
+    id: 1,
+    title: "کلینیک",
+    images: [
+      "public/baner/istockphoto-1012405368-612x612.jpg",
+      "public/baner/pexels-pixabay-40568.jpg",
+      "public/baner/workforce-feature.jpg__992x558_q85_crop-smart_subsampling-2_upscale.jpg",
+
+      "public/baner/nurses-patients-care-procedures.webp",
+      "public/baner/unnamed.jpg",
+      "public/baner/lab-quality-assurance-l.jpg",
+      "public/baner/The Importance of Water in a Laboratory.png",
+    ],
+  },
+  {
+    id: 2,
+    title: "خانواده",
+    images: [
+      "public/baner/nurses-patients-care-procedures.webp",
+      "public/baner/lab-quality-assurance-l.jpg",
+      "public/baner/unnamed.jpg",
+      "public/baner/The Importance of Water in a Laboratory.png",
+      "public/baner/pexels-pixabay-40568.jpg",
+      "public/baner/workforce-feature.jpg__992x558_q85_crop-smart_subsampling-2_upscale.jpg",
+      "public/baner/istockphoto-1012405368-612x612.jpg",
+    ],
+  },
+  {
+    id: 3,
+    title: "آزمایشگاه",
+    images: [
+      "public/baner/pexels-pixabay-40568.jpg",
+      "public/baner/workforce-feature.jpg__992x558_q85_crop-smart_subsampling-2_upscale.jpg",
+      "public/baner/unnamed.jpg",
+      "public/baner/The Importance of Water in a Laboratory.png",
+      "public/baner/lab-quality-assurance-l.jpg",
+
+      "public/baner/nurses-patients-care-procedures.webp",
+      "public/baner/istockphoto-1012405368-612x612.jpg",
+    ],
+  },
+  {
+    id: 4,
+    title: "اطفال",
+    images: [
+      "public/baner/nurses-patients-care-procedures.webp",
+      "public/baner/istockphoto-1012405368-612x612.jpg",
+      "public/baner/unnamed.jpg",
+      "public/baner/The Importance of Water in a Laboratory.png",
+      "public/baner/pexels-pixabay-40568.jpg",
+
+      "public/baner/lab-quality-assurance-l.jpg",
+      "public/baner/workforce-feature.jpg__992x558_q85_crop-smart_subsampling-2_upscale.jpg",
+    ],
+  },
+  {
+    id: 5,
+    title: "درمان",
+    images: [
+      "public/baner/The Importance of Water in a Laboratory.png",
+      "public/baner/nurses-patients-care-procedures.webp",
+      "public/baner/istockphoto-1012405368-612x612.jpg",
+      "public/baner/lab-quality-assurance-l.jpg",
+      "public/baner/workforce-feature.jpg__992x558_q85_crop-smart_subsampling-2_upscale.jpg",
+      "public/baner/unnamed.jpg",
+
+      "public/baner/pexels-pixabay-40568.jpg",
+    ],
+  },
+];
